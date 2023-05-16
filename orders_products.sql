b4fd1c7438f81d06a7fe62df4b258d722c13af94
Renardent <renard_ardente@gmail.com>
2023-05-17 01:02:50 +0300

    relational algebra

--- orders_products.sql
+++ orders_products.sql
@@ -64,8 +64,75 @@ SELECT sum(quantity), product_id
 FROM orders_to_products
 GROUP BY product_id;
 
-------------------
+--------Скільки моделей залишилось менше всього----------------
 
-SELECT brand, min(quantity)
+SELECT *
 FROM products
-GROUP BY brand; /* */
+ORDER BY quantity ASC;
+
+--------Sorting----------
+
+SELECT * FROM users
+ORDER BY id ASC;
+
+/*ascending*/
+
+SELECT * FROM users
+ORDER BY id DESC;
+
+/*descending*/
+
+UPDATE users
+SET birthday = '1940-05-12'
+WHERE id BETWEEN 5 AND 10;
+
+SELECT * FROM users
+ORDER BY birthday;
+
+SELECT * FROM users
+ORDER BY birthday ASC,
+                first_name ASC;
+
+
+/*
+
+1. Вісортувати юзерів за віком (кількістю повних років)
+2. Відсортувати телефони за ціною від найдорожчого до найдешевшого
+3. Виведіть топ-5 телефонів, які частіше за все купують
+
+*/
+
+--1
+
+SELECT extract('years' from age(birthday)),* FROM users
+ORDER BY extract('years' from age(birthday)) ;
+
+--2
+
+SELECT * FROM products
+ORDER BY price DESC;
+
+--3
+
+SELECT * FROM orders_to_products
+ORDER BY product_id DESC;
+
+
+/*
+1. Витягти всі бренди, у яких кількість телефонів > 1000
+2. Витягти всі бренди, в яких продано більше 50 тел
+*/
+
+--1
+SELECT brand, sum(quantity)
+FROM products
+GROUP BY brand
+HAVING sum(quantity) > 1000;
+
+--2
+SELECT product_id, sum(quantity)
+FROM orders_to_products
+GROUP BY product_id
+HAVING sum(quantity) > 50;
+
+----
