c43177ed415f0cb10b5736edfe04fcba6d9093fb
Renardent <renard_ardente@gmail.com>
2023-05-19 20:32:44 +0300

    NF3

--- normalizationDB.sql
+++ normalizationDB.sql
@@ -30,5 +30,60 @@ SELECT * FROM employees
 JOIN positions ON employees.position = positions.name;
 
 
-------------------
+---------NF3---------
 
+
+DROP TABLE employees;
+
+DROP TABLE positions;
+
+
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name varchar(200),
+    department varchar(200),
+    department_phone varchar(15)
+);
+
+
+INSERT INTO employees (name, department, department_phone) VALUES
+('John Doe', 'HR', '23-12-15'),
+('Jane Doe', 'Sales', '23-45-13'),
+('Carl Moe', 'Developer', '45-67-89');
+
+-----Людина залежить від департмент, департмент від телефона, а людина не від телефона, це порушення БД
+
+-----Образується надлишковість даних, дублюють номери 
+
+INSERT INTO employees (name, department, department_phone) VALUES
+('Alex Kroe', 'Developer', '45-67-89');
+
+CREATE TABLE departments (
+    name varchar(200) PRIMARY KEY,
+    phone_number varchar(15)
+);
+
+INSERT INTO departments VALUES 
+('HR', '23-12-15'),
+('Sales', '23-45-13'),
+('Developer', '45-67-89');
+
+INSERT INTO departments VALUES 
+('Manager', '21-00-15');
+
+
+ALTER TABLE employees
+DROP COLUMN department_phone;
+
+
+ALTER TABLE employees 
+ADD FOREIGN KEY (department)
+REFERENCES departments(name); 
+
+---Поки в департменс не з'явиться нова посада, її не додати в емплойс
+
+INSERT INTO employees (name, department) VALUES
+('Alex Kris', 'Manager');
+
+
+----BCNF------
