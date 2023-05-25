9eb0090525154e80eb89e6cc1aa9df5f2c30458f
Renardent <renard_ardente@gmail.com>
2023-05-25 21:13:44 +0300

    tasks with new schema

--- views and enum.sql
+++ views and enum.sql
@@ -77,6 +77,8 @@ CREATE TABLE tasks.employees (
     name varchar(100) NOT NULL CHECK (name != '')
 );
 
+----current_date - повертає текущу дату
+
 /*
 Проблеми:
 1. Відсутність ключа у юзерів. Ключем має бути мейл
@@ -84,54 +86,59 @@ CREATE TABLE tasks.employees (
 3. Зберігання паролю у сирому вигляді. Паролі мають зберігатись у захешованому вигляді (drop password + create password_hash).
 */
 
---- Всі зміни в таблицях проводити виключно ALTER-ом.
+--- Всі зміни в таблицях проводити ALTER-ом.
 
+--1+3
+ALTER TABLE tasks.user
+ADD PRIMARY KEY(email);
 
+ALTER TABLE tasks.user
+DROP COLUMN password;
 
+ALTER TABLE tasks.user
+ADD COLUMN password_hash varchar(200) NOT NULL CHECK (password_hash!='') ;
 
+--2
 
----1
-
-ALTER TABLE newtask.users
-ADD PRIMARY KEY (email);
-
-ALTER TABLE newtask.employees
+ALTER TABLE tasks.employees
 ADD COLUMN id serial PRIMARY KEY;
 
-CREATE TABLE newtask.positions(
+CREATE TABLE tasks.positions(
     department varchar(200) NOT NULL CHECK (department!=''),
-    position varchar(200) NOT NULL CHECK (position!=''),
-    PRIMARY KEY (department, position)
+    position varchar(200) NOT NULL CHECK (position!='')
 );
 
-INSERT INTO newtask.positions VALUES (
+ALTER TABLE tasks.employees
+DROP COLUMN department;
+
+ALTER TABLE tasks.employees
+DROP COLUMN position;
+
+ALTER TABLE tasks.positions 
+ADD COLUMN id serial PRIMARY KEY;
+
+INSERT INTO tasks.employees VALUES
+('3000', '1880-09-22', 'Bitter'),
+('8000','1880-09-22', 'Danna');
+
+
+INSERT INTO tasks.positions VALUES (
     'HR', 'Secretary'
 );
 
-ALTER TABLE newtask.positions
-DROP CONSTRAINT "positions_pkey";
-
-ALTER TABLE newtask.positions 
-ADD COLUMN id serial PRIMARY KEY;
-
-ALTER TABLE newtask.positions 
+ALTER TABLE tasks.positions 
 ADD CONSTRAINT "unique_pair" UNIQUE(department, position);
 
-ALTER TABLE newtask.employees
-DROP COLUMN department;
+ALTER TABLE tasks.employees
+ADD COLUMN position_id int REFERENCES tasks.positions(id);
 
-ALTER TABLE newtask.employees
-DROP COLUMN position;
-
-ALTER TABLE newtask.employees
-ADD COLUMN position_id int REFERENCES newtask.positions(id);
-
-
-
-ALTER TABLE newtask.employees
+ALTER TABLE task.employees
 ADD COLUMN dismissal_date date DEFAULT current_date 
 CHECK (dismissal_date>=hire_date);
 
+UPDATE tasks.employees
+SET position_id = 1
+WHERE name = 'Danna';
 
 /*
 
